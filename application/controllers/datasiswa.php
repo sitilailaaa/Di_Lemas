93af2ebe5f76ed90167b350690192738576d78e3
sitilailaaa <145761236+sitilailaaa@users.noreply.github.com>
2023-11-05 20:42:45 +0700

    Update datasiswa.php

--- application/controllers/datasiswa.php
+++ application/controllers/datasiswa.php
@@ -1,23 +1,33 @@
 <?php
-class datasiswa extends CI_Controller
-{
- public function index()
- {
- $this->load->view('view-form-datasiswa');
- }
- public function cetak()
- {
- $datasiswa = [
- 'Nama Siswa' => $this->input->post('Nama Siswa'),
- 'NIS' => $this->input->post('NIS'),
- 'Kelas' => $this->input->post('Kelas'),
- 'Tanggal Lahir' => $this->input->post('Tanggal Lahir'),
- 'Tempat Lahir' => $this->input->post('Tempat Lahir'),
- 'alamat' => $this->input->post('alamat'),
- 'Jenis Kelamin' => $this->input->post('Jenis Kelamin'),
- 'Agama' => $this->input->post('Agama')
- ];
- $this->load->view('view-data-datasiswa',$datasiswa);
-  
-}
-}
+    class datasiswa extends CI_Controller 
+    {
+        public function index()
+        {
+            $this->load->view("view-form-datasiswa");
+        }
+        public function cetak()
+        {
+            $this->form_validation->set_rules("nama","nama siswa","required|min_length[3]", [
+                "required"=> "Nama Siswa Wajib diisi",
+                "min_lenght"=> "Nama Terlalu pendek",]);
+            $this->form_validation->set_rules("nis","nis","required|min_length[3]",[
+                "required"=> "Nis Wajib diisi",
+                "min_lenght"=> "Nis Terlalu pendek",]);
+            if ($this->form_validation->run() != true) {
+                $this->load->view("form-input");
+            }else{
+                $data_siswa =[
+                    "nama" => $this->input->post("nama"),
+                    "nis" => $this->input->post("nis"),
+                    "kelas" => $this->input->post("kelas"),
+                    "tanggal"=> $this->input->post("tanggal"),
+                    "tempat"=> $this->input->post("tempat"),
+                    "alamat"=> $this->input->post("alamat"),
+                    "jk"=> $this->input->post("jk"),
+                    "agama"=> $this->input->post("agama"),
+                ];
+                $this->load->view("view-datasiswa", $data_siswa);
+            }
+        }   
+    }
+?>
