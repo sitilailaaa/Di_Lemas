c6fc78908e8b70ddebd9fdb6a1be13711d233c44
sitilailaaa <145761236+sitilailaaa@users.noreply.github.com>
2023-11-05 20:43:28 +0700

    Update view-form-datasiswa.php

--- application/views/view-form-datasiswa.php
+++ application/views/view-form-datasiswa.php
@@ -1,94 +1,97 @@
 <html>
-<head>
-    <title>Form Input Datasiswa</title>
-</head>
-<body>
-    <center>
-        <form action="<?= base_url('datasiswa/cetak'); ?>"method="post">
-        <table>
-            <tr>
-                <th colspan="8"> Form Input Data Siswa</th>
-            </tr>
-            <tr>
-                <td colspan="8">
-                    <hr>
-                </td>
-            </tr>
-            <tr>
-                <th>Nama Siswa</th>
-                <td>:</td>
-                <td>
-                    <input type="text" name="Nama Siswa" id="namasiswa">
-                </td>
-            </tr>
-            <tr>
-                <th>NIS</th>
-                <td>:</td>
-                <td>
-                    <input type="text" name="NIS" id="NIS">
-                </td>
-            </tr>
-            <tr>
-                <th>Kelas</th>
-                <td>:</td>
-                <td>
-                    <input type="text" name="Kelas" id="Kelas">
-                </td>
-            </tr>
-            <tr>
-                <th>Tanggal Lahir</th>
-                <td>:</td>
-                <td>
-                    <input type="text" name="Tanggal Lahir" id="tanggallahir">
-                </td>
-            </tr>
-            <tr>
-                <th>Tempat Lahir</th>
-                <td>:</td>
-                <td>
-                    <input type="text" name="Tempat Lahir" id="tempatlahir">
-                </td>
-            </tr>
-            <tr>
-                <th>Alamat</th>
-                <td>:</td>
-                <td><input type="text" name="Alamat" id="alamat"></td>
-            </tr>
-            <tr>
-                <th>Jenis Kelamin</th>
-                <td>:</td>
-                <td>
-                    <input type="radio">
-                    <label for="Laki-Laki">Laki-Laki</label>
-                    
-                    <input type="radio">
-                    <label for="Perempuan">Perempuan</label>
-                </td>
-            </tr>
-            <tr>
-                <th>Agama</th>
-                <td>:</td>
-                <td>
-                    <select name="Agama" id="Agama">
-                        <option value="">Pilih</option>
-                        <option value="Islam">Islam</option>
-                        <option value="Kristen">Kristen</option>
-                        <option value="Katolik">Katolik</option>
-                        <option value="Budha">Budha</option>
-                        <option value="Hindu">Kristen</option>
-                        <option value="Protestan">Protestan</option>
-                        <option value="Khonghucu">Khonghucu</option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="8" align="center">
-                    <input type="submit" value="Submit">
-
-                </td>
-            </tr>
-        </table>
-        </form>
-    </center>
-</body>
+    <head>
+        <title>Form Input Data Siswa</title>
+    </head>
+    <body>
+        <center>
+                <form action="<?= base_url("datasiswa/cetak") ?>" method="post">
+                    <table>
+                        <tr>
+                            <th colspan="3">
+                                Form Input Data Siswa
+                            </th>
+                        </tr>
+                        <tr>
+                            <th colspan="3">
+                                <hr>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>Nama Siswa</th>
+                            <th>:</th>
+                            <td>
+                                <input type="text" name="nama" id="nama">
+                            </td>
+                        </tr>
+                        <?= form_error("nama");?>
+                        <tr>
+                            <th>Nis</th>
+                            <th>:</th>
+                            <td>
+                                <input type="text" name="nis" id="nis">
+                            </td>
+                        </tr>
+                        <?= form_error("nis");?>
+                        <tr>
+                            <th>Kelas</th>
+                            <th>:</th>
+                            <td>
+                                <input type="text" name="kelas" id="kelas">
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Tanggal Lahir</th>
+                            <th>:</th>
+                            <td>
+                                <input type="text" name="tanggal" id="tanggal">
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Tempat Lahir</th>
+                            <th>:</th>
+                            <td>
+                                <input type="text" name="tempat" id="tempat">
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Alamat</th>
+                            <th>:</th>
+                            <td>
+                                <input type="text" name="alamat" id="alamat">
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Jenis Kelamin</th>
+                            <td>:</td>
+                            <td>
+                                <input type="radio" name="jk" value="Laki-Laki">Laki-Laki
+                                <input type="radio" name="jk" value="Perempuan">Perempuan
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Agama</th>
+                            <td>:</td>
+                            <td>
+                                <select name="agama" id="agama">
+                                    <option value="">Pilih Agama</option>
+                                    <option value="Islam">Islam</option>
+                                    <option value="Kristen">Kristen</option>
+                                    <option value="Katolik">katolik</option>
+                                    <option value="Budha">Budha</option>
+                                    <option value="Hindu">Hindu</option>
+                                    <option value="Protestan">Protestan</option>
+                                    <option value="Khonghucu">Khonghucu</option>
+                                    <option value="Ateis">Ateis</option>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="3" align="center">
+                                <input type="SUBMIT" value="SUBMIT">
+                            </td>
+                        </tr>
+                    </table>
+                </form>
+        </center>
+    </body>
 </html>
